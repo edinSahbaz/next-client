42b73d1ae72c86c34e41350225650dc1400682a4
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-04-01 13:01:45 +0200

    Policy done

--- components/general/Container.tsx
+++ components/general/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Container = ({ children } : { children : ReactNode }) => {
     return ( 
-        <div className="px-[15%] py-12 flex flex-col items-center justify-center gap-8">
+        <div className="px-[15%] py-12 flex flex-col items-center justify-center gap-8 text-[var(--p-txt-color)]">
             { children }
         </div>
      );
--- pages/privatnost/index.tsx
+++ pages/privatnost/index.tsx
@@ -5,6 +5,11 @@ import Link from "next/link";
 import { BiInfoCircle } from "react-icons/bi";
 import { FaCreditCard } from "react-icons/fa";
 
+interface ParagrpahProps {
+    title: string;
+    description: string;
+}
+
 const VerticalLine = () => (
     <div className="w-1 bg-[var(--p-txt-color)] rounded-l-sm"></div>
 )
@@ -28,10 +33,17 @@ const PersonalInfo = () => (
                 <li>1. Idite na vaš profil, odnosno 
                     <Link href="/profil" className="ml-1 text-[var(--sec-txt-color)] font-[700] hover-underline-animation hover-underline-animation-red">stranicu za profil</Link>.
                 </li>
-                <li>2. Idite do sekcije <span className="font-[700]">Brisanje Profila</span>.</li>
+                <li>2. Idite do sekcije <span className="font-[700]">Brisanje profila</span>.</li>
                 <li>3. Kliknite na dugme <span className="font-[700]">Obriši profil</span>.</li>
             </ol>
         </div>
+    </div>
+)
+
+const Paragrpah = ({ title, description }: ParagrpahProps) => (
+    <div>
+        <h2 className="text-xl font-semibold">{title}</h2>
+        <p className="">{description}</p>
     </div>
 )
 
@@ -59,6 +71,44 @@ const Privacy = () => {
 
                 <Container>
                     <PersonalInfo />
+
+                    <div className="w-full flex flex-col gap-4">
+                        <h2 className="text-[var(--title-txt-color)] my-4 text-3xl">Politika privatnosti</h2>
+
+                        <Paragrpah 
+                            title="Uvod"
+                            description="Zaštita vaših privatnih informacija je naš prioritet. 
+                            Ova Izjava o privatnosti se odnosi na website nauciProgramiranje.ba i regulira prikupljanje i korištenje podataka. 
+                            Korištenjem nauciProgramiranje.ba web stranice, pristajete na prakse prikupljanja podataka opisane u ovoj izjavi." 
+                        />
+
+                        <Paragrpah 
+                            title="Prikupljanje vaših ličnih podataka"
+                            description="Ako kupite proizvode i usluge nauciProgramiranje.ba, prikupljamo informacije o naplati i kreditnoj kartici. 
+                            Ove informacije se koriste za dovršenje transakcije kupovine. 
+                            nauciProgramiranje.ba vas potiče da pregledate izjave o privatnosti web stranica na koje se povezujete s nauciProgramiranje.ba kako biste razumjeli kako te web stranice prikupljaju, 
+                            koriste i dijele vaše informacije. nauciProgramiranje.ba nije odgovoran za izjave o privatnosti ili druge sadržaje na web stranicama izvan web stranice nauciProgramiranje.ba." 
+                        />
+
+                        <Paragrpah 
+                            title="Promjene ove izjave"
+                            description="nauciProgramiranje.ba će povremeno ažurirati ovu izjavu o privatnosti kako bi odrazila povratne informacije tvrtke i kupaca. 
+                            nauciProgramiranje.ba vas ohrabruje da povremeno pregledate ovu izjavu kako biste bili informirani o tome kako nauciProgramiranje.ba štiti vaše informacije." 
+                        />
+                        
+                        <Paragrpah
+                            title="Sigurnost vaših osobnih informacija"
+                            description="nauciProgramiranje.ba osigurava vaše osobne informacije od neovlaštenog pristupa, upotrebe ili otkrivanja. 
+                            nauciProgramiranje.ba koristi SSL protokol u tu svrhu: Kad se osobne informacije (poput broja kreditne kartice) prenose na druge web stranice, 
+                            štite se upotrebom enkripcije - SSL protokola(Secure Sockets Layer)."
+                        />
+
+                        <Paragrpah
+                            title="Kontakt informacije"
+                            description="nauciProgramiranje.ba pozdravlja vaša pitanja ili komentare u vezi s ovom izjavom o privatnosti. Ako vjerujete da nauciProgramiranje.ba nije 
+                            poštovao ovu izjavu, molimo kontaktirajte nauciProgramiranje.ba tim slanjem poruke putem web stranice."
+                        />
+                    </div>
                 </Container>
             </main>
         </> 
