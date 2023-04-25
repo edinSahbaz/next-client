23c52b0ab75d2d22e395fa95f2495ca2262b4aa5
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-04-25 20:58:40 +0200

    Team section

--- pages/tim/index.tsx
+++ pages/tim/index.tsx
@@ -1,7 +1,45 @@
 import Container from "@/components/general/Container";
 import PageDetails from "@/components/header/PageDetails";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { FaCreditCard } from "react-icons/fa";
+
+interface Member{
+    name: string;
+    position: string;
+    imgLink: string;
+    description: string;
+    social: {
+        github: string;
+        linkedin: string;
+    }
+}
+
+const TeamMember = ({ name, position, imgLink, description, social }: Member) => (
+    <div className="w-[calc(50%-2rem)] flex flex-col gap-2 justify-center items-center">
+        <h2 className="text-4xl text-[var(--title-txt-color)]">{name}</h2>
+        <p className="text-xl">{position}</p>
+
+        <div className="rounded-full w-fit shadow-2xl my-6 relative">
+            <Image width={220} height={220} alt={name} src={imgLink}
+            className="rounded-full" />
+
+            <div className="absolute flex items-center gap-2 -right-8 -bottom-2">
+                <Link href={social.github} target="_blank">
+                    <BsGithub className="text-3xl text-black hover:text-gray-700 transition-all duration-300" />
+                </Link>
+
+                <Link href={social.linkedin} target="_blank">
+                    <BsLinkedin className="text-3xl text-[#0072b1] hover:text-[#215e7f] transition-all duration-300" />
+                </Link>
+            </div>
+        </div>
+
+        <p className="text-justify leading-7 font-[400]">{description}</p>
+    </div>
+)
 
 const Team = () => {
     return ( 
@@ -27,7 +65,29 @@ const Team = () => {
             </main>
 
             <Container>
-                
+                <div className="w-full flex flex-wrap gap-16 items-center justify-center">
+                    <TeamMember 
+                        name="Edin Šahbaz" 
+                        position="Osnivač i CEO" 
+                        imgLink="/team/EdinSahbaz.jpeg"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        social={{
+                            github: "https://github.com/edinSahbaz",
+                            linkedin: "https://www.linkedin.com/in/edinsahbaz/"
+                        }}
+                    />
+
+                    <TeamMember 
+                        name="John Doe" 
+                        position="Predavač i instruktor" 
+                        imgLink="/team/blank.jpeg"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        social={{
+                            github: "https://github.com",
+                            linkedin: "https://www.linkedin.com"
+                        }}
+                    />
+                </div>
             </Container>
         </>
      );
