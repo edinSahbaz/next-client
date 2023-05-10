a80ed50885bd2fe93ad9e6f7f4600d426a7dff9c
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-05-10 17:43:37 +0200

    Intro section start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,74 @@
 import Container from '@/components/general/Container';
 import PageDetails from '@/components/header/PageDetails'
 import Head from 'next/head'
+import { ReactNode } from 'react';
+import { BiDesktop } from 'react-icons/bi';
 import { FaCreditCard } from 'react-icons/fa';
 
 export default function Home() {
+    interface IntroElementProps {
+        title: string;
+        description: string;
+        link: string;
+        icon: ReactNode;
+    }
+
+    const IntroElement = ({title, description, link, icon}: IntroElementProps) => (
+        <div>
+            {icon}
+
+            <h3 className=''>{title}</h3>
+            <p className=''>{description}</p>
+            
+            <a href={link} className=''>
+                Pogledaj više.
+            </a>
+        </div>
+    )
+
+    const Intro = () => {
+        const iconStyle = "text-4xl text-[var(--sec-txt-color)]";
+        
+        const cards: IntroElementProps[] = [
+            {
+                title: 'Pogodnosti platforme',
+                description: 'Učenje programiranja je nesumnjivo zastrašujuće. nauciProgramiranje.ba vam pruža najjednostavniju, najmoderniju platformu, sa svime što vam je potrebno da naučite programiranje lako i bez stresa.',
+                link: '',
+                icon: <BiDesktop className={iconStyle}/>
+            },
+            {
+                title: 'Pogodnosti platforme',
+                description: 'Učenje programiranja je nesumnjivo zastrašujuće. nauciProgramiranje.ba vam pruža najjednostavniju, najmoderniju platformu, sa svime što vam je potrebno da naučite programiranje lako i bez stresa.',
+                link: '',
+                icon: <BiDesktop className={iconStyle}/>
+            },
+            {
+                title: 'Pogodnosti platforme',
+                description: 'Učenje programiranja je nesumnjivo zastrašujuće. nauciProgramiranje.ba vam pruža najjednostavniju, najmoderniju platformu, sa svime što vam je potrebno da naučite programiranje lako i bez stresa.',
+                link: '',
+                icon: <BiDesktop className={iconStyle}/>
+            },
+        ]
+
+        return (
+            <div>
+                <h2>Šta je nauciProgramiranje.ba?</h2>
+
+                {
+                    cards.map((card, index) => (
+                        <IntroElement
+                            key={index}
+                            title={card.title}
+                            description={card.description}
+                            link={card.link}
+                            icon={card.icon}
+                        />
+                    ))
+                }
+            </div>
+        );
+    }
+    
     return (
         <>
             <Head>
@@ -26,7 +91,7 @@ export default function Home() {
                 />
 
                 <Container>
-                    
+                    <Intro />
                 </Container>
             </main>
         </>
