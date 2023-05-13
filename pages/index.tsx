3b934c594e001042c3559bc5af36a4617dc9a741
Edin Šahbaz <edin.sahbaz@hotmail.com>
2023-05-13 09:24:05 +0200

    Intro done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Container from '@/components/general/Container';
 import PageDetails from '@/components/header/PageDetails'
 import Head from 'next/head'
+import Link from 'next/link';
 import { ReactNode } from 'react';
-import { BiDesktop } from 'react-icons/bi';
+import { AiOutlineDatabase } from 'react-icons/ai';
+import { BiBook, BiCodeBlock, BiDesktop, BiMovie } from 'react-icons/bi';
+import { BsPersonCheck } from 'react-icons/bs';
 import { FaCreditCard } from 'react-icons/fa';
+import { MdChecklistRtl } from 'react-icons/md';
+import { TbCertificate } from 'react-icons/tb';
 
 export default function Home() {
     interface IntroElementProps {
@@ -14,20 +19,21 @@ export default function Home() {
     }
 
     const IntroElement = ({title, description, link, icon}: IntroElementProps) => (
-        <div>
+        <div className='flex flex-col items-center gap-2 hover:cursor-pointer hover:shadow-xl border-[1px] border-transparent 
+        hover:border-gray-100 w-[calc(33.3%-1rem)] p-6 transition-all duration-300 text-center'>
             {icon}
 
-            <h3 className=''>{title}</h3>
-            <p className=''>{description}</p>
+            <h3 className='text-[var(--title-txt-color)] text-xl'>{title}</h3>
+            <p className='text-[var(--p-txt-color)] text-[15px]'>{description}</p>
             
-            <a href={link} className=''>
+            <Link href={link} className='hover-underline-animation hover-underline-animation-red text-[var(--sec-txt-color)]'>
                 Pogledaj više.
-            </a>
+            </Link>
         </div>
     )
 
     const Intro = () => {
-        const iconStyle = "text-4xl text-[var(--sec-txt-color)]";
+        const iconStyle = "text-[3rem] text-[var(--sec-txt-color)]";
         
         const cards: IntroElementProps[] = [
             {
@@ -37,34 +43,68 @@ export default function Home() {
                 icon: <BiDesktop className={iconStyle}/>
             },
             {
-                title: 'Pogodnosti platforme',
-                description: 'Učenje programiranja je nesumnjivo zastrašujuće. nauciProgramiranje.ba vam pruža najjednostavniju, najmoderniju platformu, sa svime što vam je potrebno da naučite programiranje lako i bez stresa.',
+                title: 'Sadržajan kurikulum',
+                description: 'Ispravno, pravo učenje programiranja zahtijeva detaljno planiran i kreiran kurikulum fokusiran na sve bitne elemente programiranja, što je upravo ono čime se nauciProgramiranje.ba može pohvaliti.',
                 link: '',
-                icon: <BiDesktop className={iconStyle}/>
+                icon: <BiBook className={iconStyle}/>
             },
             {
-                title: 'Pogodnosti platforme',
-                description: 'Učenje programiranja je nesumnjivo zastrašujuće. nauciProgramiranje.ba vam pruža najjednostavniju, najmoderniju platformu, sa svime što vam je potrebno da naučite programiranje lako i bez stresa.',
+                title: 'Dizajnirano od strane stručnjaka',
+                description: 'Programiranje možete učiti iz mnogih resursa i izvora ili možete učiti iz kurikuluma koji su sastavili i kreirali stručnjaci, programeri, sa dugogodišnjim iskustvom u IT industriji i podučavanju progamiranja.',
                 link: '',
-                icon: <BiDesktop className={iconStyle}/>
+                icon: <BsPersonCheck className={iconStyle}/>
+            },
+            {
+                title: 'Instrukcije visokog kvaliteta',
+                description: 'Učenje programiranja mnogo je lakše i mnogo ugodnije kada imate pristup videima i meet-ovima visoke kvalitete gdje je objašnjen svaki detalj potreban da shvatite neki koncpet.',
+                link: '',
+                icon: <BiMovie className={iconStyle}/>
+            },
+            {
+                title: 'Pitanja za provjeru',
+                description: 'Praksa čini savršenim, posebno kada se uči programirati. Većina naših lekcija sadrži pitanja za vježbu kako biste učvrstili vaše razumijevanje svake teme. To su stotine pitanja koja će vas učiniti profesionalcem.',
+                link: '',
+                icon: <MdChecklistRtl className={iconStyle}/>
+            },
+            {
+                title: 'Velika baza zadataka',
+                description: 'Super je znati programirati. Još je zgodnije znati kako napraviti sjajne projekte uz programiranje. Naših 5 praktičnih projekata programiranja će vas provesti kroz to i približiti pravom svijetu programiranja.',
+                link: '',
+                icon: <AiOutlineDatabase className={iconStyle}/>
+            },
+            {
+                title: 'Vrhunsko radno okruženje',
+                description: 'U svojoj srži, programiranje je primijenjena vještina. Zato sve naše lekcije dolaze sa radnim okruženjem za programiranje bogatim funkcijama gdje možete pisati i izvršavati kod, otklanjati greške i čuvati kod.',
+                link: '',
+                icon: <BiCodeBlock className={iconStyle}/>
+            },
+            {
+                title: 'Certifikat',
+                description: 'Ako ste uspješno uradili sve zadatke i pitanja sa platforme, onda vi definitivno znate programirati! Iz tog razloga zaslužujete certifikat koji potvrđuje vaše stečeno znanje iz programiranja.',
+                link: '',
+                icon: <TbCertificate className={iconStyle}/>
             },
         ]
 
         return (
             <div>
-                <h2>Šta je nauciProgramiranje.ba?</h2>
+                <h2 className='text-4xl text-[var(--title-txt-color)] text-center my-10'>
+                    Šta je nauciProgramiranje.ba?
+                </h2>
 
-                {
-                    cards.map((card, index) => (
-                        <IntroElement
-                            key={index}
-                            title={card.title}
-                            description={card.description}
-                            link={card.link}
-                            icon={card.icon}
-                        />
-                    ))
-                }
+                <div className='flex flex-wrap gap-6 items-center justify-center'>
+                    {
+                        cards.map((card, index) => (
+                            <IntroElement
+                                key={index}
+                                title={card.title}
+                                description={card.description}
+                                link={card.link}
+                                icon={card.icon}
+                            />
+                        ))
+                    }
+                </div>
             </div>
         );
     }
